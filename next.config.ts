1e003aca3057d4d50a04e6c482e580ec22fb53e6
lingeshnafeax <wlingesh260@gmail.com>
2025-07-29 22:37:58 +0530

    Chore: Add standalone build

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
 };
 const withNextIntl = createNextIntlPlugin("./lib/i8n.ts");
 export default withNextIntl(nextConfig);
