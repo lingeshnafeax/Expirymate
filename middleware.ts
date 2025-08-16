31229b0015968afe63e17c8d0fa5b7853d1f8fe7
lingeshnafeax <wlingesh260@gmail.com>
2025-08-16 21:17:23 +0530

    Refactor: Middleware to handle login page redirect

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,38 @@
 import { NextRequest, NextResponse } from "next/server";
-import { auth } from "./lib/auth/better-auth";
-import { headers } from "next/headers";
-import { Roles } from "./types/types";
+import { AppRoutes, Roles } from "./types/types";
 import { ACCESS_CONTROL, APP_ROUTES } from "./static/constants/constants";
+import { getUserServerSession } from "./utils/server";
 
 export default async function middleware(request: NextRequest) {
-  const userSession = await auth.api.getSession({
-    headers: await headers(),
-  });
+  try {
+    const userSession = await getUserServerSession();
 
-  //? Check if user is authenticated
-  if (!userSession?.session || !userSession.user) {
+    const { pathname } = request.nextUrl;
+    const isLoggedIn = !!(userSession?.session && userSession.user);
+
+    if (pathname === APP_ROUTES.LOGIN && isLoggedIn) {
+      return NextResponse.redirect(new URL(APP_ROUTES.HOME, request.url));
+    }
+
+    if (!isLoggedIn && pathname === APP_ROUTES.HOME) {
+      return NextResponse.redirect(new URL(APP_ROUTES.LOGIN, request.url));
+    }
+
+    if (isLoggedIn && userSession?.user.role) {
+      const role = userSession.user.role as Roles;
+      if (!ACCESS_CONTROL[role].includes(pathname)) {
+        return NextResponse.redirect(
+          new URL(APP_ROUTES.FORBIDDEN, request.url),
+        );
+      }
+    }
+
+    return NextResponse.next();
+  } catch {
     return NextResponse.redirect(new URL(APP_ROUTES.LOGIN, request.url));
   }
-  //? Check if user has correct permission
-  if (userSession?.user.role) {
-    const user = userSession.user.role as Roles;
-
-    if (!ACCESS_CONTROL[user].includes(request.nextUrl.pathname)) {
-      return NextResponse.redirect(new URL(APP_ROUTES.FORBIDDEN, request.url));
-    }
-  }
-
-  //TODO Find a workaround for this
-  //   if (
-  //     request.nextUrl.pathname === APP_ROUTES.LANDING ||
-  //     request.nextUrl.pathname === APP_ROUTES.LOGIN
-  //   ) {
-  //     return NextResponse.redirect(new URL(APP_ROUTES.HOME, request.url));
-  //   }
-  //   console.log("Request path", request.nextUrl.pathname);
-
-  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/home"],
+  matcher: ["/home", "/login"] satisfies AppRoutes[],
 };
